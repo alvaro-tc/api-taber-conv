0deabc6a949813ab60b120fcd9284a3297752c70
Alvarashion <alvarotorrezc@gmail.com>
2024-12-09 15:51:13 -0400

    GET users actualizado

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -126,10 +126,7 @@ def get_all_users():
     claims = get_jwt()
 
     if claims.get("admin") == True:
-        page = request.args.get("page", default=1, type=int)
-        per_page = request.args.get("per_page", default=3, type=int)
-        
-        users = User.query.paginate(page=page, per_page=per_page)
+        users = User.query.all()
         result = UserSchema().dump(users, many=True)
         
         return (
