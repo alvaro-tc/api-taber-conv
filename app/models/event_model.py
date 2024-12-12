97192861db865e7952bd7b0477b37a7d8e66ff01
Alvarashion <alvarotorrezc@gmail.com>
2024-12-12 10:26:13 -0400

    partially fixed bugs event_model

--- app/models/event_model.py
+++ app/models/event_model.py
@@ -20,7 +20,7 @@ class Event(db.Model):
 
     def save(self):
         if self.estado == 0 and self.descripcion in ["Asistencia Mañana", "Asistencia Tarde"]:
-            existing_event = Event.query.filter_by(estado=0).first()
+            existing_event = Event.query.filter_by(estado=0, descripcion=self.descripcion).first()
             if existing_event:
                 raise ValueError(f"Ya existe un evento con estado 0")
         db.session.add(self)
@@ -28,7 +28,7 @@ class Event(db.Model):
 
     def update(self, descripcion, estado, fecha):
         if estado == 0 and self.estado != 0 and descripcion in ["Asistencia Mañana", "Asistencia Tarde"]:
-            existing_event = Event.query.filter_by(estado=0).first()
+            existing_event = Event.query.filter_by(estado=0, descripcion=descripcion).first()
             if existing_event:
                 raise ValueError("Ya existe un evento con estado 0 y la misma descripción")
         self.descripcion = descripcion
